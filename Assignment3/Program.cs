da8ff0a01c3a8c092b7e81760e1df70c6b7ea1ae
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-24 23:36:03 +0300

    Question2

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -5,14 +5,24 @@
         static void Main(string[] args)
         {
             #region Question 1
-             //Create a one-dimensional array double[] prices with the values
-             //25.5, 40.0, 33.75.
-             //Print the second price (index 1).
-             
+            //Create a one-dimensional array double[] prices with the values
+            //25.5, 40.0, 33.75.
+            //Print the second price (index 1).
+
 
             //double[] prices = { 25.5, 40.0, 33.75 };
             //Console.WriteLine(prices[1]);
 
+            #endregion
+
+            #region Question2
+            //int[,] shelfCopies =
+            //{
+            //    {3,5},
+            //    {1,4}
+            //};
+
+            //Console.WriteLine(shelfCopies[1, 0]);
             #endregion
         }
     }
