a1434480a92b257c76aa04326f959049dea57ab1
AbdullahMohamedVP1 <boda12834@gmail.com>
2026-07-25 22:54:22 +0300

    Question 9

--- Assignment3/Program.cs
+++ Assignment3/Program.cs
@@ -118,6 +118,36 @@ namespace Assignment3
 
             //Console.WriteLine(prices.Length);
             #endregion
+
+            #region Question 9
+            //Write a method bool TryGetPrice(string title, out double price) that returns true and sets
+            //price to 25.5 if title is "Clean Code", otherwise returns false and sets price to 0.Call it
+
+            //static bool TryGetPrice(string title, out double price)
+            //{
+            //    if (title == "Clean Code")
+            //    {
+            //        price = 25.5;
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        price = 0;
+            //        return false;
+            //    }
+            //}
+
+            //double price;
+
+            //if (TryGetPrice("Clean Code", out price))
+            //{
+            //    Console.WriteLine(price);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Book not found");
+            //}
+            #endregion
         }
     }
 }
